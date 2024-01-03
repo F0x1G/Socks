4a0576888972edb756dc58820df49aaab4606eae
F0x1G <92969226+F0x1G@users.noreply.github.com>
2024-01-03 13:01:18 +0200

    Explorer
    Додав проводник, щоб завантажувати фото з інших папок(сира версія)

--- src/main/java/com/example/socks/HelloController.java
+++ src/main/java/com/example/socks/HelloController.java
@@ -1,11 +1,24 @@
 package com.example.socks;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button Load;
+    @FXML
+    private Button Start;
+    @FXML
+    private Button Redo;
+    @FXML
+    private Button Bucket;
+    @FXML
+    private Button Save;
+    @FXML
+    private Button Test;
 
     @FXML
     protected void onHelloButtonClick() {
