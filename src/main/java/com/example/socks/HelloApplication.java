7a708882b44b89d71f32d4660b95f896dbe267e7
F0x1G <92969226+F0x1G@users.noreply.github.com>
2024-01-13 15:13:40 +0200

    Maximized on open

--- src/main/java/com/example/socks/HelloApplication.java
+++ src/main/java/com/example/socks/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        stage.setMaximized(true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
