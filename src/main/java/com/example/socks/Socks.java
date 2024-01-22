5ff4cb4cc1f894fa78d2af4ebdb5b4c2c10a9372
astalaD <stopitsmi@gmail.com>
2024-01-22 21:38:30 +0200

    shinish beta

---
+++ src/main/java/com/example/socks/Socks.java
@@ -0,0 +1,9 @@
+package com.example.socks;
+
+import java.io.IOException;
+
+public class Socks {
+    public static void main(String[] args) throws IOException {
+        HelloApplication.main(args);
+    }
+}
