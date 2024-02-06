d6784df563c6e9c53922073f977b2bf2e056506a
astalaD <stopitsmi@gmail.com>
2024-02-06 14:49:42 +0200

    pre relize

--- src/main/java/com/example/socks/Socks.java
+++
@@ -1,9 +0,0 @@
-package com.example.socks;
-
-import java.io.IOException;
-
-public class Socks {
-    public static void main(String[] args) throws IOException {
-        HelloApplication.main(args);
-    }
-}
